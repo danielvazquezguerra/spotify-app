1c32f6501c20a8f554c46c960b061241cc2a9947
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-08-19 13:40:07 +0200

    getQuery ok

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -14,25 +14,33 @@ export class SpotifyService {
     private http: HttpClient,
     ) { }
 
+      getQuery(query:string) {
 
-getNewReleases() {
-  const URL:string = 'https://api.spotify.com/v1/browse/new-releases'
-  const headers = new HttpHeaders({
-    'Authorization':`Bearer ${this.BEARER}`
-  })
-  return this.http.get( URL, { headers } )
-    .pipe(map(data => data['albums'].items));
-}
+        const URL:string = `https://api.spotify.com/v1/${ query }`;
+        const headers = new HttpHeaders({
+          'Authorization':`Bearer ${this.BEARER}`
+        })
 
-getArtist(termino:string) {
+        return this.http.get(URL, { headers });
 
-  const URL_SEARCH:string = `https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`;
-  const headers = new HttpHeaders({
-    'Authorization':`Bearer ${this.BEARER}`
-  })
-  return this.http.get( URL_SEARCH, { headers } )
-    .pipe(map( data => data['artists'].items));
-}
+
+      }
+
+      getNewReleases() {
+
+        return this.getQuery('browse/new-releases?limit=20')
+          .pipe(map(data => data['albums'].items));
+      }
+
+      getArtist(termino:string) {
+
+        const URL_SEARCH:string = `https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`;
+        const headers = new HttpHeaders({
+          'Authorization':`Bearer ${this.BEARER}`
+        })
+        return this.http.get( URL_SEARCH, { headers } )
+          .pipe(map( data => data['artists'].items));
+      }
 
 
 }
